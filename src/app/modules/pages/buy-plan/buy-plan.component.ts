9437d1f91505875a171ac2ada6005de44af59215
Arul Ananth <arulananth05@gmail.com>
2023-07-07 01:51:29 +0530

    Price BTC
    Price BTC

--- src/app/modules/pages/buy-plan/buy-plan.component.ts
+++ src/app/modules/pages/buy-plan/buy-plan.component.ts
@@ -5,7 +5,8 @@ import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { ApiService } from 'src/app/shared/services/api.service';
 import { AuthService } from 'src/app/shared/services/auth.service';
-
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-buy-plan',
   templateUrl: './buy-plan.component.html',
@@ -15,7 +16,9 @@ export class BuyPlanComponent implements OnInit {
 
   paymenyForm: FormGroup | any;
   priceData: any;
-
+  btcPrice:number=0;
+  ethPrice:number=0;
+  walletAddress:string=''
   constructor(
     @Inject(MAT_DIALOG_DATA) public modeldata: any,
     private dialogRef: MatDialogRef<
@@ -24,16 +27,27 @@ export class BuyPlanComponent implements OnInit {
     private toastr: ToastrService,
     private route: Router,
     public formBuilder: FormBuilder,
-    private auth: AuthService
+    private auth: AuthService,
+    private http:HttpClient
   ) {
     let data=modeldata.price;
-    console.log('data--',data);
     this.priceData = data;
+    this.walletAddress= environment.walletAddress;
+     this.http.request('GET', 'https://min-api.cryptocompare.com/data/price?fsym=USD&tsyms=BTC,ETH', {responseType:'json'}).subscribe((msg:any)=>{
+        if(msg && msg.BTC)
+        {
+          this.btcPrice =  parseFloat(msg.BTC)*parseFloat(this.priceData.price)
+        }
+        if(msg && msg.ETH)
+        {
+          this.ethPrice =  parseFloat(msg.ETH)*parseFloat(this.priceData.price)
+        }
+     })
   }
 
   ngOnInit(): void {
     this.paymenyForm = this.formBuilder.group({
-      address: [''],
+      address: [this.walletAddress],
       link: ['']
     });
   }
